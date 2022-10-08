868b8f0676dfb11fb985f59ff1d82f7ca952d1c4
CharlesBeise <81447311+CharlesBeise@users.noreply.github.com>
2022-10-08 13:00:31 -0500

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,11 +1,13 @@
 import unittest
 import intro
 
+
 class TestCase(unittest.TestCase):
-  
+
     def test1(self):
         expected = "Welcome to the game!"
         self.assertEqual(intro.game_start(), expected)
-    
+
+
 if __name__ == '__main__':
     unittest.main()
