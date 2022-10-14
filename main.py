1499e459039307cd3788029dff2e0347097bf062
Andrew Blair <andrewjblair@gmail.com>
2022-10-14 16:00:21 -0700

    Add basic flow of game start and execution loop

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
-def game_start():
-    return "Welcome to the game!"
+from Classes.game import Game
+from Classes.player import Player
+
+
+saveFile = "saveStates.json"
+
+def gameStart(saveFile):
+    # Initialize Game and load state
+    game = Game(saveFile)
+    game.titleScreen()
+    game.selectGameState()
+    # TODO:: Maybe this would be a good place for story/narrative depending on new/loaded game???
+    # Begin user input loop to play game
+    while game.isRunning():
+        userInput = input("> ")
+        if userInput.replace(" ", "").lower() == "exitgame":
+            game.exitGame()
+        # Parse user input for command/action
+
+if __name__ == "__main__":
+
+    gameStart(saveFile)
