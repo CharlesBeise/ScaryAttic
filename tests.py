d9f0c410f58acff934ef26670d5cccc4bae060a7
Andrew Blair <andrewjblair@gmail.com>
2022-10-30 13:05:37 -0700

    Remove unnecessary unit test

--- tests.py
+++ tests.py
@@ -9,14 +9,8 @@ from Classes.game import Game
 class TestCase(unittest.TestCase):
 
     def test1(self):
-        # Redirect stdout to test output to console
-        testOutput = StringIO()
-        sys.stdout = testOutput
-        testFile = "testSaveStates.json"
-        testGame = Game(testFile)
-        testGame.titleScreen()
-        expected = "Welcome to the game! (placeholder)\nEnter the command 'exit game' to stop playing.\n"
-        self.assertEqual(testOutput.getvalue(), expected)
+        # Placeholder
+        pass
 
 
 if __name__ == '__main__':
