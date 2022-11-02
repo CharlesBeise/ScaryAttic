04708b474db232c0d7e4af4750fcddefb7e2537c
CharlesBeise <beisec@oregonstate.edu>
2022-11-01 23:16:57 -0500

    Added actionVerbs "take", "drop", and "inventory".

--- Language_Parser/actionVerbs.py
+++ Language_Parser/actionVerbs.py
@@ -1,9 +1,47 @@
-__all__ = ['examine', 'take']
+__all__ = ['examine', 'take', 'inventory', 'drop']
 
 
-def examine(item):
-    print("That is an interesting", item)
+def examine(info):
+    pass
 
 
-def take(item):
-    print("You have added the " + item + " to your inventory")
+def take(info):
+    game = info["Game"]
+    item = info["Items"][0]
+    player = game.getPlayer()
+    room = player.getLocation()
+    result = room.removeItem(item)
+    if result:
+        player.addInventory(result)
+        for possession in player.getInventory():
+            if possession == item:
+                print(possession.verbResponses("Take"))
+                return
+    else:
+        print("You can't seem to find that item here.")
+
+
+def drop(info):
+    """
+    This function removes an item from the player's inventory and leaves it in
+    the player's current room.
+    """
+    if len(info["Items"]) == 0:
+        print("I don't think that will work.")
+    game = info["Game"]
+    player = game.getPlayer()
+    room = player.getLocation()
+    for item in info["Items"]:
+        for possession in player.getInventory():
+            if possession == item:
+                print(possession.verbResponses("Drop"))
+                player.removeInventory(item)
+                room.addItem(possession)
+                continue
+
+
+def inventory(info):
+    response = "You currently have the following items: "
+    for item in info["Player"].getInventory():
+        response = response + item.getName() + ','
+    print(response[:-1])
